c97c15a6ca861def7d6778a783cac9896196b77b
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2023-05-30 16:35:28 +0300

    KULLANILMAYAN REFERASLARI TEMİZLENDİ.

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.Xml;
-using System.Data.SqlClient;
 
 namespace Doviz_Ofis
 {
